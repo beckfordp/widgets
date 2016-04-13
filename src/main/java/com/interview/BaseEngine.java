543da684b212f479830a163f44f4ff0cddd98ecf
beckfordp <beckfordp@btinternet.com>
2016-04-13 15:49:40 +0100

    Dry-up duplication between SteamEngine and InternalCombustionEngine.

---
+++ src/main/java/com/interview/BaseEngine.java
@@ -0,0 +1,46 @@
+package com.interview;
+
+public class BaseEngine {
+    protected boolean running;
+    protected int fuelLevel;
+    protected FuelType requiredFuelType;
+    private FuelType fuelType;
+
+    public BaseEngine(FuelType requiredFuelType) {
+        fuelLevel = 0;
+        running = false;
+        this.requiredFuelType = requiredFuelType;
+    }
+
+    public void start() {
+        if (fuelLevel > 0 && requiredFuelType.equals(fuelType)) {
+            running = true;
+        } else {
+            throw new IllegalStateException("Not able to start engine.");
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void fill(FuelType fuelType, int fuelLevel) {
+        if (fuelLevel >= 0 && fuelLevel <= 100) {
+            this.fuelLevel = fuelLevel;
+        } else if (fuelLevel > 100) {
+            this.fuelLevel = 100;
+        } else {
+            this.fuelLevel = 0;
+        }
+
+        this.fuelType = fuelType;
+    }
+
+    public FuelType getFuelType() {
+        return requiredFuelType;
+    }
+}
